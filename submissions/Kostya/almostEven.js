09b42cdc4d7e90e606e5f0f36de0c85eb8f168ef
My Name <myEmail@example.com>
2020-04-26 06:45:18 +0300

    DZ4 kata almost even

---
+++ submissions/Kostya/almostEven.js
@@ -0,0 +1,21 @@
+
+
+function finteger(chislo, chastey) {
+    let kratnost = chislo/chastey;
+    kratnost = Math.round(kratnost-0.5);
+    let ostatok = chislo - (kratnost * chastey);
+    let otvet=[];
+    for (;chastey>0; chastey--)
+        otvet.push(kratnost);
+    for (x=otvet.length; ostatok>0; ostatok--)
+    {otvet[x-1]=kratnost+1;
+        x--}
+    return otvet
+}
+console.log(finteger(20,6));
+
+
+
+
+
+
