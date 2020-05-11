27804adc3fdfd053cfd277d118ce3d7c258b2e3d
Zahar Gulchak <gulchak.zahar@gmail.com>
2020-05-11 15:52:10 +0300

    fixed issues

--- submissions/Kostya/almostEven.js
+++ submissions/Kostya/almostEven.js
@@ -1,21 +1,13 @@
-
-
 function finteger(chislo, chastey) {
-    let kratnost = chislo/chastey;
-    kratnost = Math.round(kratnost-0.5);
-    let ostatok = chislo - (kratnost * chastey);
-    let otvet=[];
-    for (;chastey>0; chastey--)
-        otvet.push(kratnost);
-    for (x=otvet.length; ostatok>0; ostatok--)
-    {otvet[x-1]=kratnost+1;
-        x--}
-    return otvet
+  let kratnost = chislo / chastey;
+  kratnost = Math.round(kratnost - 0.5);
+  let ostatok = chislo - kratnost * chastey;
+  let otvet = [];
+  for (; chastey > 0; chastey--) otvet.push(kratnost);
+  for (let x = otvet.length; ostatok > 0; ostatok--) {
+    otvet[x - 1] = kratnost + 1;
+    x--;
+  }
+  return otvet;
 }
-console.log(finteger(20,6));
-
-
-
-
-
-
+console.log(finteger(20, 6));
