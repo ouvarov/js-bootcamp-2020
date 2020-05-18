6fdc04ad20530b47618b0f93c0c7dae021b68fcc
Zahar Gulchak <gulchak.zahar@gmail.com>
2020-05-18 13:58:28 +0300

    update 09 task

--- submissions/zahar/09-user-interaction/index.js
+++ submissions/zahar/09-user-interaction/index.js
@@ -1,17 +1,62 @@
-/* eslint-disable */
-function onLoad() {
-  console.log('Загрузилось!!!');
-  prompt();
-  confirm();
-  alert();
-}
+(function (window) {
+  const _ = {};
 
-function onBlur(evt) {
-  console.log(evt.target.value);
-}
+  _.runTask = () => {
+    const answer = prompt('Who is there?');
 
-function onClick() {
-  const answer = prompt('Are you ready');
-  const paragraph = document.getElementById('prgrph');
-  paragraph.innerText = answer || 'default text';
-}
+    if (!answer) {
+      alert('Отменено');
+    } else if (answer === 'Admin') {
+      const secondAnswer = prompt('What is the password?');
+      if (!secondAnswer) {
+        alert('Отменено');
+      } else if (secondAnswer === 'Я главный') {
+        alert('Hello');
+      } else {
+        alert('Неправильный пароль');
+      }
+    } else {
+      alert('I dont know you');
+    }
+
+    return;
+  };
+
+  _.addCounter = () => {
+    const getCounter = () => {
+      let counter = 0;
+      return () => ++counter;
+    };
+
+    const counterSection = document.getElementById('counter-section');
+    const newCounterBlock = document.createElement('div');
+    const button = document.createElement('button');
+    button.innerText = 'Increment counter';
+    const counter = getCounter();
+    const label = document.createElement('label');
+    label.innerText = 0;
+    button.onclick = () => {
+      label.innerText = counter();
+    };
+    counterSection.append(newCounterBlock);
+    newCounterBlock.append(button);
+    newCounterBlock.append(label);
+  };
+
+  _.getFibonacchiNumber = () => {
+    const fibFunc = (num) => {
+      if (num === 0) {
+        return 0;
+      } else if (num === 1) {
+        return 1;
+      } else if (typeof num === 'number' && num > 0) {
+        return fibFunc(num - 1) + fibFunc(num - 2);
+      }
+    };
+    document.getElementById('fibonacchi-output').innerText = fibFunc(
+      +document.getElementById('fib-value').value,
+    );
+  };
+
+  window._ = _;
+})(window);
